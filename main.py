3224adc64958b10091e340c0f3dcaf2e8f1abd14
neverix <nev@neverix.io>
2023-07-09 02:57:29 +0300

    Make colors work

--- main.py
+++ main.py
@@ -52,6 +52,32 @@ console = Console()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def pil_to_b64(image):
+    # Convert PIL image to bytes
+    bytes = io.BytesIO()
+    image.save(bytes, format="PNG")
+    image = bytes.getvalue()
+
+    # Convert to b64
+    image = base64.b64encode(image)
+    image = image.decode("utf-8")
+    app.pipe_lock = False
+    
+    return image
+
+
+def cuda_wrapper(fn):
+    def fn_(*args, **kwargs):
+        if args:
+            console.print_(args[0])
+        result = fn(*args, **kwargs)
+        t = time.perf_counter()
+        console.print(f"Generated{' ' + kwargs_outer['object_name'] if 'object_name' in kwargs_outer else ''} in",
+                      time.perf_counter() - t, "seconds")
+        return result
+    return fn_
+
+
 # == Texture generation ==
 def patch_conv(**patch):
     cls = torch.nn.Conv2d
@@ -93,31 +119,16 @@ class TextureInferenceResponse(BaseModel):
 app.pipe_lock = False
 
 
-def pil_to_b64(image):
-    # Convert PIL image to bytes
-    bytes = io.BytesIO()
-    image.save(bytes, format="PNG")
-    image = bytes.getvalue()
-
-    # Convert to b64
-    image = base64.b64encode(image)
-    image = image.decode("utf-8")
-    app.pipe_lock = False
-    
-    return image
-
-
 @app.post("/generate_texture")
 @torch.no_grad()
+@cuda_wrapper
 def generate(request: TextureInferenceRequest) -> TextureInferenceResponse:
-    while app.pipe_lock:
-        time.sleep(0.1)
-    app.pipe_lock = True
-    console.print(request)
-    t = time.perf_counter()
+#     while app.pipe_lock:
+#         time.sleep(0.1)
+#     app.pipe_lock = True
     pipe.set_sampler(request.sampler)
-    image = pipe(request.text).images[0]
-    console.print(f"Generated image in", time.perf_counter() - t, "seconds")
+    with torch.autocast("cuda"):
+        image = pipe(request.text).images[0]
     image = pil_to_b64(image)
     return TextureInferenceResponse(image=image)
 
@@ -232,7 +243,7 @@ class HfLLMInterface(object):
         self.model = AutoModelForCausalLM.from_pretrained(model_name, **load_kwargs)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    def __call__(self, prompt: List[Dict[str, str]]) -> Any:
+    def __call__(self, prompt: List[Dict[str, str]], repetition_penalty=1.1) -> Any:
         text = []
         for part in prompt:
             if part["role"] == "user":
@@ -251,7 +262,7 @@ class HfLLMInterface(object):
                 do_sample=True,
                 top_k=40,
                 top_p=0.95,
-                repetition_penalty=1.1,
+                repetition_penalty=repetition_penalty,
                 max_new_tokens=64,
                 stopping_criteria=[
                     lambda ids, *_: any(tok in self.tokenizer.decode(ids[0][-1]) for tok in ("\n", "</s>"))
@@ -365,38 +376,44 @@ class Color(BaseModel):
     colors: List[str]
     lut: str
 
-COLORS = 
-{"indianred":"#CD5C5C","lightcoral":"#F08080","salmon":"#FA8072","darksalmon":"#E9967A","lightsalmon":"#FFA07A","crimson":"#DC143C","red":"#FF0000","darkred":"#8B0000","pink":"#FFC0CB","lightpink":"#FFB6C1","hotpink":"#FF69B4","deeppink":"#FF1493","mediumvioletred":"#C71585","palevioletred":"#DB7093","coral":"#FF7F50","tomato":"#FF6347","orangered":"#FF4500","darkorange":"#FF8C00","orange":"#FFA500","gold":"#FFD700","yellow":"#FFFF00","lightyellow":"#FFFFE0","lemonchiffon":"#FFFACD","lightgoldenrodyellow":"#FAFAD2","papayawhip":"#FFEFD5","moccasin":"#FFE4B5","peachpuff":"#FFDAB9","palegoldenrod":"#EEE8AA","khaki":"#F0E68C","darkkhaki":"#BDB76B","lavender":"#E6E6FA","thistle":"#D8BFD8","plum":"#DDA0DD","violet":"#EE82EE","orchid":"#DA70D6","fuchsia":"#FF00FF","magenta":"#FF00FF","mediumorchid":"#BA55D3","mediumpurple":"#9370DB","rebeccapurple":"#663399","blueviolet":"#8A2BE2","darkviolet":"#9400D3","darkorchid":"#9932CC","darkmagenta":"#8B008B","purple":"#800080","indigo":"#4B0082","slateblue":"#6A5ACD","darkslateblue":"#483D8B","mediumslateblue":"#7B68EE","greenyellow":"#ADFF2F","chartreuse":"#7FFF00","lawngreen":"#7CFC00","lime":"#00FF00","limegreen":"#32CD32","palegreen":"#98FB98","lightgreen":"#90EE90","mediumspringgreen":"#00FA9A","springgreen":"#00FF7F","mediumseagreen":"#3CB371","seagreen":"#2E8B57","forestgreen":"#228B22","green":"#008000","darkgreen":"#006400","yellowgreen":"#9ACD32","olivedrab":"#6B8E23","olive":"#6B8E23","darkolivegreen":"#556B2F","mediumaquamarine":"#66CDAA","darkseagreen":"#8FBC8B","lightseagreen":"#20B2AA","darkcyan":"#008B8B","teal":"#008080","aqua":"#00FFFF","cyan":"#00FFFF","lightcyan":"#E0FFFF","paleturquoise":"#AFEEEE","aquamarine":"#7FFFD4","turquoise":"#40E0D0","mediumturquoise":"#48D1CC","darkturquoise":"#00CED1","cadetblue":"#5F9EA0","steelblue":"#4682B4","lightsteelblue":"#B0C4DE","powderblue":"#B0E0E6","lightblue":"#ADD8E6","skyblue":"#87CEEB","lightskyblue":"#87CEFA","deepskyblue":"#00BFFF","dodgerblue":"#1E90FF","cornflowerblue":"#6495ED","royalblue":"#4169E1","blue":"#0000FF","mediumblue":"#0000CD","darkblue":"#00008B","navy":"#00008B","midnightblue":"#191970","cornsilk":"#FFF8DC","blanchedalmond":"#FFEBCD","bisque":"#FFE4C4","navajowhite":"#FFDEAD","wheat":"#F5DEB3","burlywood":"#DEB887","tan":"#D2B48C","rosybrown":"#BC8F8F","sandybrown":"#F4A460","goldenrod":"#DAA520","darkgoldenrod":"#B8860B","peru":"#CD853F","chocolate":"#D2691E","saddlebrown":"#8B4513","sienna":"#A0522D","brown":"#A52A2A","maroon":"#800000","white":"#FFFFFF","snow":"#FFFAFA","honeydew":"#F0FFF0","mintcream":"#F5FFFA","azure":"#F0FFFF","aliceblue":"#F0F8FF","ghostwhite":"#F8F8FF","whitesmoke":"#F5F5F5","seashell":"#FFF5EE","beige":"#F5F5DC","oldlace":"#FDF5E6","floralwhite":"#FDF5E6","ivory":"#FFFFF0","antiquewhite":"#FAEBD7","linen":"#FAF0E6","lavenderblush":"#FFF0F5","mistyrose":"#FFE4E1","gainsboro":"#DCDCDC","lightgray":"#D3D3D3","silver":"#C0C0C0","darkgray":"#A9A9A9","gray":"#808080","dimgray":"#696969","lightslategray":"#778899","slategray":"#708090","darkslategray":"#2F4F4F","black":"#000000"}
+COLORS = {"indianred":"#CD5C5C","lightcoral":"#F08080","salmon":"#FA8072","darksalmon":"#E9967A","lightsalmon":"#FFA07A","crimson":"#DC143C","red":"#FF0000","darkred":"#8B0000","pink":"#FFC0CB","lightpink":"#FFB6C1","hotpink":"#FF69B4","deeppink":"#FF1493","mediumvioletred":"#C71585","palevioletred":"#DB7093","coral":"#FF7F50","tomato":"#FF6347","orangered":"#FF4500","darkorange":"#FF8C00","orange":"#FFA500","gold":"#FFD700","yellow":"#FFFF00","lightyellow":"#FFFFE0","lemonchiffon":"#FFFACD","lightgoldenrodyellow":"#FAFAD2","papayawhip":"#FFEFD5","moccasin":"#FFE4B5","peachpuff":"#FFDAB9","palegoldenrod":"#EEE8AA","khaki":"#F0E68C","darkkhaki":"#BDB76B","lavender":"#E6E6FA","thistle":"#D8BFD8","plum":"#DDA0DD","violet":"#EE82EE","orchid":"#DA70D6","fuchsia":"#FF00FF","magenta":"#FF00FF","mediumorchid":"#BA55D3","mediumpurple":"#9370DB","rebeccapurple":"#663399","blueviolet":"#8A2BE2","darkviolet":"#9400D3","darkorchid":"#9932CC","darkmagenta":"#8B008B","purple":"#800080","indigo":"#4B0082","slateblue":"#6A5ACD","darkslateblue":"#483D8B","mediumslateblue":"#7B68EE","greenyellow":"#ADFF2F","chartreuse":"#7FFF00","lawngreen":"#7CFC00","lime":"#00FF00","limegreen":"#32CD32","palegreen":"#98FB98","lightgreen":"#90EE90","mediumspringgreen":"#00FA9A","springgreen":"#00FF7F","mediumseagreen":"#3CB371","seagreen":"#2E8B57","forestgreen":"#228B22","green":"#008000","darkgreen":"#006400","yellowgreen":"#9ACD32","olivedrab":"#6B8E23","olive":"#6B8E23","darkolivegreen":"#556B2F","mediumaquamarine":"#66CDAA","darkseagreen":"#8FBC8B","lightseagreen":"#20B2AA","darkcyan":"#008B8B","teal":"#008080","aqua":"#00FFFF","cyan":"#00FFFF","lightcyan":"#E0FFFF","paleturquoise":"#AFEEEE","aquamarine":"#7FFFD4","turquoise":"#40E0D0","mediumturquoise":"#48D1CC","darkturquoise":"#00CED1","cadetblue":"#5F9EA0","steelblue":"#4682B4","lightsteelblue":"#B0C4DE","powderblue":"#B0E0E6","lightblue":"#ADD8E6","skyblue":"#87CEEB","lightskyblue":"#87CEFA","deepskyblue":"#00BFFF","dodgerblue":"#1E90FF","cornflowerblue":"#6495ED","royalblue":"#4169E1","blue":"#0000FF","mediumblue":"#0000CD","darkblue":"#00008B","navy":"#00008B","midnightblue":"#191970","cornsilk":"#FFF8DC","blanchedalmond":"#FFEBCD","bisque":"#FFE4C4","navajowhite":"#FFDEAD","wheat":"#F5DEB3","burlywood":"#DEB887","tan":"#D2B48C","rosybrown":"#BC8F8F","sandybrown":"#F4A460","goldenrod":"#DAA520","darkgoldenrod":"#B8860B","peru":"#CD853F","chocolate":"#D2691E","saddlebrown":"#8B4513","sienna":"#A0522D","brown":"#A52A2A","maroon":"#800000","white":"#FFFFFF","snow":"#FFFAFA","honeydew":"#F0FFF0","mintcream":"#F5FFFA","azure":"#F0FFFF","aliceblue":"#F0F8FF","ghostwhite":"#F8F8FF","whitesmoke":"#F5F5F5","seashell":"#FFF5EE","beige":"#F5F5DC","oldlace":"#FDF5E6","floralwhite":"#FDF5E6","ivory":"#FFFFF0","antiquewhite":"#FAEBD7","linen":"#FAF0E6","lavenderblush":"#FFF0F5","mistyrose":"#FFE4E1","gainsboro":"#DCDCDC","lightgray":"#D3D3D3","silver":"#C0C0C0","darkgray":"#A9A9A9","gray":"#808080","dimgray":"#696969","lightslategray":"#778899","slategray":"#708090","darkslategray":"#2F4F4F","black":"#000000"}
 @app.post("/gen_color")
+# @cuda_wrapper
 def gen_color(request: GenerateColorRequest) -> Color:
+    res, res_high = 17, 17
+    grid = np.stack(np.mgrid[0:res, 0:res, 0:res], -1)[..., ::-1]
+    
     colors = llms["tiny"]([
         {"role": "system", "content": 'A chat between a curious human ("User") and an '
          'artificial intelligence assistant ("Generator"). The assistant generates themes '
          'for video games. The assistant is able to output 3 colors '
          'like red, green and blue for each theme.'},
-    ] + [msg for msg in ({"role": "user", "content": theme + "?"},
-                         {"role": "assistant", "content": ", ".join(colors)}) for theme, colors in [
+    ] + [msg for theme, cs in [
         ("Underwater", ("cyan", "black", "blue")),
         ("Jungle", ("green", "brown", "black")),
         ("Space", ("blue", "black", "white")),
         ("Circus", ("red", "yellow", "white")),
         ("Book", ("brown", "black", "white")),
         ("Halloween", ("orange", "green", "purple")),
-    ]]).split(", ")
-    hexes = [COLORS[name] for name in colors]
-    rgbs = [[int("".join(col), 16) / 255 for col in chunked(color[1:], 2)] for color in hexes]
-    rgbs = np.asarray(rgbs)
-    std = request.sigma
-    dists = []
-    for rgb in rgbs:
-        dists.append(truncnorm(-rgb * std, (1 - rgb) * std))
-    res = 17
-    grid = np.stack(np.mgrid[0:res, 0:res, 0:res], -1)[..., ::-1]
-    res_high = 17
-    samps = np.stack(np.mgrid[0:res_high, 0:res_high, 0:res_high], -1).reshape(-1, 3) / (res_high - 1)
-    cdfs = np.mean([dist.cdf((samps - rgb) * std) for dist, rgb in zip(dists, rgbs)], 0)
-    attn = ((grid.reshape(-1, 3)[:, None, :] / (res - 1) - cdfs[None, :, :]) ** 2).sum(-1).argmin(-1)
-    new_lut = samps[attn].reshape(grid.shape)
+    ] for msg in ({"role": "user", "content": request.theme + "?"},
+                         {"role": "assistant", "content": ", ".join(cs)})], repetition_penalty=1.0).split(",")
+    colors = [name.strip().lower() for name in colors]
+    console.print("Colors generated:", colors)
+    hexes = [COLORS[name] for name in colors if name in COLORS]
+    if not hexes:
+        new_lut = grid
+    else:
+        rgbs = [[int("".join(col), 16) / 255 for col in chunked(color[1:], 2)] for color in hexes]
+        rgbs = np.asarray(rgbs)
+        std = request.sigma
+        dists = []
+        for rgb in rgbs:
+            dists.append(truncnorm(-rgb * std, (1 - rgb) * std))
+        samps = np.stack(np.mgrid[0:res_high, 0:res_high, 0:res_high], -1).reshape(-1, 3) / (res_high - 1)
+        cdfs = np.mean([dist.cdf((samps - rgb) * std) for dist, rgb in zip(dists, rgbs)], 0)
+        attn = ((grid.reshape(-1, 3)[:, None, :] / (res - 1) - cdfs[None, :, :]) ** 2).sum(-1).argmin(-1)
+        samps = samps[attn]
+        new_lut = samps.reshape(grid.shape)
     pic = Image.fromarray(np.concatenate(new_lut * 255, 1).astype("uint8"))
     return Color(colors=colors, lut=pil_to_b64(pic))
 
@@ -429,8 +446,12 @@ def gen_world(request: GenerateworldRequest) -> World:
                     " condition, there must be a treasure object in the world."
                     " Do not place a player object in the world, as this"
                     " will be handled by the game engine."
-                    "Here is an example world:"
-                    + json.dumps(
+                    + "\n The name of each object must be unique. The metadata"
+                    " can be any JSON object. Only output the JSON object.",
+                },
+                {"role": "user", "content": "Generate the world for this description: Woodchuck game"},
+                {"role": "assistant", "content":
+                    json.dumps(
                         {
                             "objects": [
                                 {
@@ -447,12 +468,33 @@ def gen_world(request: GenerateworldRequest) -> World:
                             ]
                         }
                     )
-                    + "\n The name of each object must be unique. The metadata"
-                    " can be any JSON object. Only output the JSON object.",
+                },
+                {"role": "user", "content": "Generate the world for this description: Cooking"},
+                {"role": "assistant", "content": '{"objects": [{"name": "kitchen", "metadata": {"has_oven": true}}, {"name": "player", "metadata": {"skill_level": 5, "current_recipe": "soup"}}], "game_state": {"recipes": {"soup": {"prep_time": 10, "cook_time": 20}, "chicken_parmesan": {"prep_time": 45, "cook_time": 30}}}}'},
+                {"role": "user", "content": "Generate the world for this description: Jungle"},
+                {"role": "assistant", "content":
+                    json.dumps({
+                        "objects": [
+                          {"name": "tree", "metadata": {"color": "green"}},
+                          {"name": "bush", "metadata": {"color": "green"}},
+                          {"name": "river", "metadata": {"color": "blue"}},
+                          {"name": "rock", "metadata": {"color": "gray"}},
+                          {"name": "game_state", "metadata": {
+                              "win_conditions": {
+                                "find_treasure": false,
+                                "escape_jungle": false
+                              },
+                              "fail_conditions": {
+                                "died_from_hunger": false,
+                                "attacked_by_wild_animal": false
+                              }
+                            }
+                          }
+                        ]})
                 },
                 {
                     "role": "user",
-                    "content": "Generate the world" "for the description:" + world_desc,
+                    "content": "Generate the world for the description:" + world_desc,
                 },
             ],
         )
